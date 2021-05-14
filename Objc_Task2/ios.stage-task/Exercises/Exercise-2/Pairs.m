913e74d2ff7208d005cbdde48eacb26ce32efa75
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2021-05-15 01:11:40 +0300

    fix: ex 2 Pairs

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,23 +3,38 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    NSInteger count = 0;
-    NSInteger length = [array count];
+
+//    algorithm wit some mistakes:
+//    NSInteger count = 0;
+//    NSInteger length = [array count];
+//
+//    NSInteger i = 0;
+//    NSInteger j = 1;
+//
+//    while (j <= length - 1) {
+//        if (array[j].intValue - array[i].intValue == number.intValue) {
+//            count++;
+//            j++;
+//            i++;
+//        }
+//        else if (array[j].intValue - array[i].intValue < number.intValue && j != length) {
+//            j++;
+//        }
+//        else if (array[j].intValue - array[i].intValue > number.intValue && j != length) {
+//            i++;
+//        }
+//    }
     
-    NSInteger i = 0;
-    NSInteger j = 1;
-    
-    while (j <= length - 1) {
-        if (array[j].intValue - array[i].intValue == number.intValue) {
-            count++;
-            j++;
-            i++;
-        }
-        else if (array[j].intValue - array[i].intValue < number.intValue && j != length) {
-            j++;
-        }
-        else if (array[j].intValue - array[i].intValue > number.intValue && j != length) {
-            i++;
+    if (array.count < 2)
+    {
+        return 0;
+    }
+    int count = 0;
+    for (NSUInteger i = 0; i < array.count; i++) {
+        for (NSUInteger j = i+1; j< array.count; j++) {
+            if ([array objectAtIndex:j].intValue - [array objectAtIndex:i].intValue == number.intValue) {
+                count++;
+            }
         }
     }
     return count;
